97f5e93becf6b8f4e051cc723650256202aa0de5
Thang Bui Quang <89576896+thangbuiq@users.noreply.github.com>
2024-03-20 10:19:49 +0000

    feat: implement base scaffold generator

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# actions
+# Projectopia Generator
+
+This project is a GitHub Actions workflow that uses a Python module to create new projects based on a template. It leverages the power of Cookiecutter, Cruft, and the GitHub API to provide a seamless project creation experience.
+
+## Features
+
+- **Workflow Dispatch Trigger**: Manually trigger the GitHub Actions workflow whenever you need to create a new project.
+- **Cookiecutter**: Use Cookiecutter to define project templates that include both directory structures and file contents.
+- **Cruft**: Keep your project templates up-to-date with Cruft, which allows you to update project instances when the template changes.
+- **GitHub API**: Create new GitHub repositories for your projects directly from the workflow.
+
+## Getting Started
+
+To get started with it, view our website: [Projectopia Website](https://projectopia-frontend.vercel.app/)
+
+## Contributing
+
+Contributions to this project are welcome!
+
+## License
+
+This project is licensed under the terms of the MIT license. See the [LICENSE](LICENSE) file for details.
---
+++ projectopia/repo/__main__.py
@@ -0,0 +1,74 @@
+# Path: trigger/repo/__main__.py
+
+from projectopia.utils.github.repository import PersonalRepository as GitHub
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="GitHub Repository Operations")
+    parser.add_argument(
+        "--name",
+        type=str,
+        required=True,
+        help="Repository name",
+    )
+    parser.add_argument(
+        "--token",
+        type=str,
+        required=True,
+        help="GitHub token",
+    )
+    parser.add_argument(
+        "--branch",
+        type=str,
+        default="main",
+        help="Branch name",
+    )
+    parser.add_argument(
+        "--source",
+        type=str,
+        default="docs",
+        help="Source directory",
+    )
+    parser.add_argument(
+        "--protection",
+        type=bool,
+        default=False,
+        help="Enable branch protection",
+    )
+    parser.add_argument(
+        "--pages",
+        type=str,
+        default="False",
+        help="Enable GitHub Pages",
+    )
+    parser.add_argument(
+        "--description",
+        type=str,
+        default="A GitHub repository",
+        help="Repository description",
+    )
+    parser.add_argument(
+        "--private",
+        type=str,
+        default="public",
+        help="Repository visibility",
+    )
+    args = parser.parse_args()
+    repo = GitHub(
+        token=args.token,
+        name=args.name,
+        description=args.description,
+        private=True if args.private.lower() == "private" else False,
+        is_template=False,
+        auto_init=True
+    )
+
+    # Create the repository
+    repo.create()
+
+    if args.pages.lower() == "true":
+        repo.add_branch(branch_name="gh-pages")
+        repo.configure_github_pages(source="/", branch="gh-pages")
+
+if __name__ == "__main__":
+    main()
