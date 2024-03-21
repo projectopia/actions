3fd4764da519d93df4e88aec9d6f8e3954d36714
TuanTran0910 <21280058@student.hcmus.edu.vn>
2024-03-21 13:38:09 +0700

    add feature creating github repository using github api

---
+++ projectopia/utils/github/config.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, constr, Field
+from typing import Optional, Dict
+from projectopia.utils.github.constants import *
+
+
+class RepositoryConfig(BaseModel):
+    name: str = "projectopia"
+    description: Optional[str] = None
+    homepage: str = ""
+    private: bool = False
+    is_template: bool = False
+    auto_init: bool = True
+
+
+class APIConfig(BaseModel):
+    token: str
+    version: str = Field(pattern=r"\d{4}-\d{2}-\d{2}")
+
+
+class BranchProtectionRulesConfig(BaseModel):
+    required_status_checks: Dict | None
+    enforce_admins: bool | None
+    required_pull_request_reviews: Dict | None
+    restrictions: Dict | None
+    required_linear_history: bool = False
+    allow_force_pushes: bool = False
+    allow_deletions: bool = False
+    block_creations: bool = False
+    required_conversation_resolution: bool = False
+    lock_branch: bool = False
+    allow_fork_syncing: bool = False
+
+
+class GithubPagesConfig(BaseModel):
+    source: Dict
+    cname: str = None
